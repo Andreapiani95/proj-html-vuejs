f246b0454c474a24868514d026cd39f7da63b395
Andreapiani95 <andreapiani1995@gmail.com>
2023-07-24 16:24:40 +0200

    initialization

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script >
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+};
+
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "scss/style.scss"
+</style>
